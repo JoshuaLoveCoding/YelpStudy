322cdd5887189af170b8824f094c952ccaf13887
JoshuaLoveCoding <anqi6591@gwu.edu>
2018-12-01 08:00:03 -0500

    Initial Commit

---
+++ YelpStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YelpStudy
+//
+//  Created by Joshua on 12/1/18.
+//  Copyright © 2018 Joshua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YelpStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YelpStudy
+//
+//  Created by Joshua on 12/1/18.
+//  Copyright © 2018 Joshua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
