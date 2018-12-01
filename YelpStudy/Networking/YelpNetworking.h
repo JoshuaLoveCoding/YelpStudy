fcdac7441e1bfe0d908a0d19a270a91e03732971
JoshuaLoveCoding <anqi6591@gwu.edu>
2018-12-01 08:07:48 -0500

    first commit

---
+++ YelpStudy/Networking/YelpNetworking.h
@@ -0,0 +1,17 @@
+//
+//  YelpNetworking.h
+//  YelpStudy
+//
+//  Created by Joshua on 12/1/18.
+//  Copyright © 2018 Joshua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YelpNetworking : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YelpStudy/Networking/YelpNetworking.m
@@ -0,0 +1,13 @@
+//
+//  YelpNetworking.m
+//  YelpStudy
+//
+//  Created by Joshua on 12/1/18.
+//  Copyright © 2018 Joshua. All rights reserved.
+//
+
+#import "YelpNetworking.h"
+
+@implementation YelpNetworking
+
+@end
